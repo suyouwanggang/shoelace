0a191feff7d62511cd22cac9be0ae99688d4eb5d
wanggang <wanggang@etppm.com>
2021-09-27 10:53:52 +0800

    doAnimate

--- src/directives/hideOrShowAnimate.ts
+++ src/directives/hideOrShowAnimate.ts
@@ -7,106 +7,106 @@ const showTransitionDuration = 300;
 const hideTransitionDuration = 300;
 
 export type HideShowOption = {
-    skip?: boolean;
-    hideRemove?: boolean;
-    onComplete?: (element: HTMLElement) => void;
-    duration?: number;
+  skip?: boolean;
+  hideRemove?: boolean;
+  onComplete?: (element: HTMLElement) => void;
+  duration?: number;
 };
 const emitEvent = (el: Element) => {
-    el.dispatchEvent(new CustomEvent('sl-animate-complete'));
+  el.dispatchEvent(new CustomEvent('sl-animate-complete'));
 };
 class HideDirective extends Directive {
-    render(_option: HideShowOption = {}) {
-        return nothing;
-    }
-    update(_part: ElementPart, [_option]: Parameters<this['render']>) {
-        new Promise(() =>
-            requestAnimationFrame(() => {
-                debugger;
-                const element = _part.element as HTMLElement;
-                const currentHeight = parseInt(getCssValue(element, 'height'));
-                const div = document.createElement('div');
-                div.style.height = currentHeight + 'px';
-                div.style.width = parseInt(getCssValue(element, 'width')) + 'px';
-                element.parentElement?.insertBefore(div, element);
+  render(_option: HideShowOption = {}) {
+    return nothing;
+  }
+  update(_part: ElementPart, [_option]: Parameters<this['render']>) {
+    new Promise(() =>
+      requestAnimationFrame(() => {
+        debugger;
+        const element = _part.element as HTMLElement;
+        const currentHeight = parseInt(getCssValue(element, 'height'));
+        const div = document.createElement('div');
+        div.style.height = currentHeight + 'px';
+        div.style.width = parseInt(getCssValue(element, 'width')) + 'px';
+        element.parentElement?.insertBefore(div, element);
 
-                animateTo(element, shimKeyframesHeightAuto(animate_hide, currentHeight), {
-                    easing: 'ease-out',
-                    duration: (_option && _option.duration) || hideTransitionDuration
-                }).then(() => {
-                    requestAnimationFrame(() => {
-                        element.parentElement?.removeChild(div);
-                        emitEvent(element);
-                        if (_option && _option.onComplete) {
-                            _option.onComplete(element);
-                        }
-                        if (_option && _option?.hideRemove) {
-                            element.parentElement?.removeChild(element);
-                        }
-                    });
-                });
-            })
-        );
-        return nothing;
-    }
+        animateTo(element, shimKeyframesHeightAuto(animate_hide, currentHeight), {
+          easing: 'ease-out',
+          duration: (_option && _option.duration) || hideTransitionDuration
+        }).then(() => {
+          requestAnimationFrame(() => {
+            element.parentElement?.removeChild(div);
+            emitEvent(element);
+            if (_option && _option.onComplete) {
+              _option.onComplete(element);
+            }
+            if (_option && _option?.hideRemove) {
+              element.parentElement?.removeChild(element);
+            }
+          });
+        });
+      })
+    );
+    return nothing;
+  }
 }
 
 class ShowDirective extends Directive {
-    render(_option?: HideShowOption) {
-        return nothing;
-    }
-    update(_part: ElementPart, [_option]: Parameters<this['render']>) {
-        new Promise(() =>
-            requestAnimationFrame(() => {
-                const element = _part.element as HTMLElement;
-                const currentHeight = parseInt(getCssValue(element, 'height'));
-                animateTo(element, shimKeyframesHeightAuto(animate_show, currentHeight), {
-                    duration: (_option && _option.duration) || showTransitionDuration
-                }).then(() => {
-                    emitEvent(element);
-                    if (_option && _option.onComplete) {
-                        _option.onComplete(element);
-                    }
-                });
-            })
-        );
-        return nothing;
-    }
+  render(_option?: HideShowOption) {
+    return nothing;
+  }
+  update(_part: ElementPart, [_option]: Parameters<this['render']>) {
+    new Promise(() =>
+      requestAnimationFrame(() => {
+        const element = _part.element as HTMLElement;
+        const currentHeight = parseInt(getCssValue(element, 'height'));
+        animateTo(element, shimKeyframesHeightAuto(animate_show, currentHeight), {
+          duration: (_option && _option.duration) || showTransitionDuration
+        }).then(() => {
+          emitEvent(element);
+          if (_option && _option.onComplete) {
+            _option.onComplete(element);
+          }
+        });
+      })
+    );
+    return nothing;
+  }
 }
 
 export type AnimateOption = {
-    name: string;
-    onComplete?: (element: HTMLElement) => void;
-    duration: number;
+  name: string;
+  onComplete?: (element: HTMLElement) => void;
+  duration: number;
 };
 
 class AnimateDirective extends Directive {
-    render(_option: AnimateOption) {
-        return nothing;
+  render(_option: AnimateOption) {
+    return nothing;
+  }
+  update(_part: ElementPart, [_option]: Parameters<this['render']>) {
+    if (!_option || !_option.name) {
+      return;
     }
-    update(_part: ElementPart, [_option]: Parameters<this['render']>) {
-        if (!_option || !_option.name) {
-            return;
+    new Promise(() =>
+      requestAnimationFrame(() => {
+        const element = _part.element as HTMLElement;
+        let frames = (animations as any)[_option.name];
+        if (!frames) {
+          return;
         }
-        new Promise(() =>
-            requestAnimationFrame(() => {
-                const element = _part.element as HTMLElement;
-                let frames = (animations as any)[_option.name];
-                if (!frames) {
-                    return;
-                }
-                animateTo(element, (animations as any)[_option.name], {
-                    duration: (_option && _option.duration) || showTransitionDuration
-                }).then(() => {
-                    emitEvent(element);
-                    if (_option && _option.onComplete) {
-                        _option.onComplete(element);
-                    }
-                });
-            })
-        );
-        return nothing;
-    }
+        animateTo(element, (animations as any)[_option.name], {
+          duration: (_option && _option.duration) || showTransitionDuration
+        }).then(() => {
+          emitEvent(element);
+          if (_option && _option.onComplete) {
+            _option.onComplete(element);
+          }
+        });
+      })
+    );
+    return nothing;
+  }
 }
 
 /**
@@ -114,7 +114,7 @@ class AnimateDirective extends Directive {
  * ```js
  * html`<div ${hide()}> element</div>`
  * html`<div ${show()}> element</div>`
- * html`<div ${dAnimate({name:''})}> element</div>`
+ * html`<div ${dAnimate({name:'slidInUp'})}> element</div>`
  */
 export const hide = directive(HideDirective);
 export const show = directive(ShowDirective);
